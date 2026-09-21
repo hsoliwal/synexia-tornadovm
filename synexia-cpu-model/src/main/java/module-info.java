46d195ff450bec71abdfb40800d3eb8874df0b64
Hitesh Soliwal <hsoliwal@gmail.com>
2026-09-21 11:40:10 +0530

    cpu-model: add module-info.java

---
+++ synexia-cpu-model/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2026 Synexia contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
+module synexia.cpu.model {
+    requires transitive tornado.api;
+
+    exports com.synexia.tornadovm.cpu;
+}

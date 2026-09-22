0b973fea2af40cde93811827a445043dcbeb68a4
Hitesh Soliwal <hsoliwal@gmail.com>
2026-09-22 08:28:44 +0530

    cpu-model: add primitive compiled block program

---
+++ synexia-cpu-model/src/main/java/com/synexia/tornadovm/cpu/RiscV32BlockProgram.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2026 Synexia contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
+package com.synexia.tornadovm.cpu;
+
+import uk.ac.manchester.tornado.api.types.arrays.Int8Array;
+import uk.ac.manchester.tornado.api.types.arrays.IntArray;
+import uk.ac.manchester.tornado.api.types.arrays.LongArray;
+
+/**
+ * Primitive-only compiled basic-block program.
+ *
+ * <p>A halfword-indexed entry map points to packed block descriptors; descriptors point into one
+ * contiguous 64-bit micro-op stream. No object is allocated per block or per operation.
+ */
+public final class RiscV32BlockProgram {
+
+    private final IntArray blockBySlot;
+    private final LongArray blockDescriptors;
+    private final Int8Array blockValid;
+    private final LongArray microOps;
+    private final int blockCount;
+    private final int operationCount;
+
+    RiscV32BlockProgram(IntArray blockBySlot, LongArray blockDescriptors, Int8Array blockValid,
+            LongArray microOps, int blockCount, int operationCount) {
+        this.blockBySlot = blockBySlot;
+        this.blockDescriptors = blockDescriptors;
+        this.blockValid = blockValid;
+        this.microOps = microOps;
+        this.blockCount = blockCount;
+        this.operationCount = operationCount;
+    }
+
+    public IntArray blockBySlot() {
+        return blockBySlot;
+    }
+
+    public LongArray blockDescriptors() {
+        return blockDescriptors;
+    }
+
+    public Int8Array blockValid() {
+        return blockValid;
+    }
+
+    public LongArray microOps() {
+        return microOps;
+    }
+
+    public int blockCount() {
+        return blockCount;
+    }
+
+    public int operationCount() {
+        return operationCount;
+    }
+
+    static long descriptor(int firstOperation, int operationCount) {
+        return ((long) firstOperation << 32) | (operationCount & 0xffffL);
+    }
+
+    public static int firstOperation(long descriptor) {
+        return (int) (descriptor >>> 32);
+    }
+
+    public static int operationCount(long descriptor) {
+        return (int) descriptor & 0xffff;
+    }
+}

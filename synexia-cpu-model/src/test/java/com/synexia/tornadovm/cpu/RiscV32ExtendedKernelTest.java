5be16438173996e5ad698148672ed36423761fef
Hitesh Soliwal <hsoliwal@gmail.com>
2026-09-22 07:27:29 +0530

    cpu-model: test RV32C A-extension and M-mode execution

---
+++ synexia-cpu-model/src/test/java/com/synexia/tornadovm/cpu/RiscV32ExtendedKernelTest.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2026 Synexia contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
+package com.synexia.tornadovm.cpu;
+
+import static com.synexia.tornadovm.cpu.RiscV32Assembler.addi;
+import static com.synexia.tornadovm.cpu.RiscV32Assembler.amoAddW;
+import static com.synexia.tornadovm.cpu.RiscV32Assembler.csrrs;
+import static com.synexia.tornadovm.cpu.RiscV32Assembler.csrrw;
+import static com.synexia.tornadovm.cpu.RiscV32Assembler.ebreak;
+import static com.synexia.tornadovm.cpu.RiscV32Assembler.ecall;
+import static com.synexia.tornadovm.cpu.RiscV32Assembler.lrW;
+import static com.synexia.tornadovm.cpu.RiscV32Assembler.mret;
+import static com.synexia.tornadovm.cpu.RiscV32Assembler.scW;
+import static com.synexia.tornadovm.cpu.RiscV32Assembler.wfi;
+import static com.synexia.tornadovm.cpu.RiscV32CompressedAssembler.cAddi;
+import static com.synexia.tornadovm.cpu.RiscV32CompressedAssembler.cEbreak;
+import static com.synexia.tornadovm.cpu.RiscV32CompressedAssembler.cLi;
+import static com.synexia.tornadovm.cpu.RiscV32CompressedAssembler.cLwsp;
+import static com.synexia.tornadovm.cpu.RiscV32CompressedAssembler.cNop;
+import static com.synexia.tornadovm.cpu.RiscV32CompressedAssembler.cSwsp;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RiscV32ExtendedKernelTest {
+
+    private final JvmRiscV32Executor executor = new JvmRiscV32Executor();
+
+    @Test
+    public void compressedArithmeticLoadStoreAndHalt() {
+        RiscV32Machine machine = new RiscV32Machine(4, 512);
+        machine.loadHalfwordsAll(0,
+                cLi(8, 5),
+                cAddi(8, 3),
+                cSwsp(8, 0),
+                cLwsp(9, 0),
+                cEbreak());
+
+        for (int core = 0; core < machine.cores(); core++) {
+            machine.register(core, 2, 128);
+        }
+
+        RiscV32ExecutionResult result = executor.execute(machine, 32, 4);
+
+        assertTrue(result.allStopped());
+        for (int core = 0; core < machine.cores(); core++) {
+            assertEquals(8, machine.register(core, 8));
+            assertEquals(8, machine.register(core, 9));
+            assertEquals(8, machine.readWord(core, 128));
+            assertEquals(RiscV32.STATUS_HALTED, machine.status(core));
+        }
+    }
+
+    @Test
+    public void thirtyTwoBitInstructionMayStartAtHalfwordBoundary() {
+        RiscV32Machine machine = new RiscV32Machine(1, 256);
+        int wide = addi(5, 0, 123);
+        machine.loadHalfwordsAll(0,
+                cNop(),
+                wide & 0xffff,
+                wide >>> 16,
+                cEbreak());
+
+        executor.execute(machine, 16, 2);
+
+        assertEquals(123, machine.register(0, 5));
+        assertEquals(RiscV32.STATUS_HALTED, machine.status(0));
+        assertEquals(8, machine.pc(0));
+    }
+
+    @Test
+    public void machineCsrsRoundTrip() {
+        RiscV32Machine machine = new RiscV32Machine(1, 256);
+        machine.loadProgramAll(0,
+                addi(1, 0, 64),
+                csrrw(2, RiscV32.CSR_MTVEC, 1),
+                csrrs(3, RiscV32.CSR_MTVEC, 0),
+                ebreak());
+
+        executor.execute(machine, 32, 2);
+
+        assertEquals(0, machine.register(0, 2));
+        assertEquals(64, machine.register(0, 3));
+        assertEquals(64, machine.csr(0, RiscV32.CSR_MTVEC));
+        assertEquals(RiscV32.MISA_RV32_IMAC, machine.csr(0, RiscV32.CSR_MISA));
+        assertEquals(0, machine.csr(0, RiscV32.CSR_MHARTID));
+    }
+
+    @Test
+    public void ecallVectorsToMtvecAndMretReturns() {
+        RiscV32Machine machine = new RiscV32Machine(1, 512)
+                .withTrapVectoring(true);
+
+        machine.loadProgramAll(0,
+                ecall(),
+                addi(5, 5, 1),
+                ebreak());
+
+        machine.loadProgram(0, 64,
+                csrrs(1, RiscV32.CSR_MEPC, 0),
+                addi(1, 1, 4),
+                csrrw(0, RiscV32.CSR_MEPC, 1),
+                mret());
+
+        machine.csr(0, RiscV32.CSR_MTVEC, 64);
+        executor.execute(machine, 64, 4);
+
+        assertEquals(1, machine.register(0, 5));
+        assertEquals(RiscV32.STATUS_HALTED, machine.status(0));
+        assertEquals(RiscV32.TRAP_ECALL_M_MODE, machine.trapCause(0));
+        assertEquals(4, machine.csr(0, RiscV32.CSR_MEPC));
+        assertEquals(RiscV32.TRAP_ECALL_M_MODE, machine.csr(0, RiscV32.CSR_MCAUSE));
+    }
+
+    @Test
+    public void atomicReadModifyWriteAndReservation() {
+        RiscV32Machine machine = new RiscV32Machine(1, 512);
+        machine.writeWord(0, 128, 7);
+        machine.loadProgramAll(0,
+                addi(1, 0, 128),
+                addi(2, 0, 5),
+                amoAddW(3, 1, 2),
+                lrW(4, 1),
+                addi(5, 0, 99),
+                scW(6, 1, 5),
+                scW(7, 1, 2),
+                ebreak());
+
+        executor.execute(machine, 64, 4);
+
+        assertEquals(7, machine.register(0, 3));
+        assertEquals(12, machine.register(0, 4));
+        assertEquals(0, machine.register(0, 6));
+        assertEquals(1, machine.register(0, 7));
+        assertEquals(99, machine.readWord(0, 128));
+    }
+
+    @Test
+    public void wfiCanBeResumedWithoutResettingArchitecturalState() {
+        RiscV32Machine machine = new RiscV32Machine(1, 256);
+        machine.loadProgramAll(0,
+                wfi(),
+                addi(10, 10, 1),
+                ebreak());
+
+        executor.execute(machine, 16, 2);
+        assertEquals(RiscV32.STATUS_WAITING, machine.status(0));
+        assertEquals(4, machine.pc(0));
+
+        machine.resumeWaitingCores();
+        executor.execute(machine, 16, 2);
+
+        assertEquals(1, machine.register(0, 10));
+        assertEquals(RiscV32.STATUS_HALTED, machine.status(0));
+    }
+}

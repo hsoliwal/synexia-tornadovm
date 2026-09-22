99ff58f0c98312da5eadb6394104fe425d2d2911
Hitesh Soliwal <hsoliwal@gmail.com>
2026-09-22 08:36:50 +0530

    cpu-model: wire exact tier budgets into JvmRiscV32Executor.java

--- synexia-cpu-model/src/main/java/com/synexia/tornadovm/cpu/JvmRiscV32Executor.java
+++ synexia-cpu-model/src/main/java/com/synexia/tornadovm/cpu/JvmRiscV32Executor.java
@@ -23,7 +23,7 @@ public final class JvmRiscV32Executor implements RiscV32Executor {
             RiscV32Kernel.runQuantum(machine.registers(), machine.pc(), machine.status(), machine.trapCause(),
                     machine.trapValue(), machine.retiredInstructions(), machine.csrs(), machine.reservations(),
                     machine.memory(), machine.decodedInstructions(), machine.decodedRawInstructions(),
-                    machine.decodedInstructionLengths(), machine.blockValid(), machine.tierFallbackMask(), 0,
+                    machine.decodedInstructionLengths(), machine.blockValid(), machine.tierFallbackBudget(), 0,
                     machine.codeCacheBase(), machine.codeCacheEnd(),
                     machine.wordsPerCore(), machine.executionFlags(), instructionsPerQuantum);
             quanta++;
